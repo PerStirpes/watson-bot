edbd1166ad217b7e97266737c899e110fcc79f59
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-08-04 11:19:08 -0700

    setup clone

--- _app.js
+++ _app.js
@@ -1,33 +1,32 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const debug = require('debug')
 const { DRIFT_VERIFICATION_TOKEN } = process.env
 const { handleMessage } = require('./lib/incoming')
-// debugging tools lines 10 - 18
-const Raven = require('raven')
-Raven.config(
-  'https://ef897a7e139c44b6a597c6b6a65147bc@sentry.io/1249642'
-).install()
-app.use(Raven.requestHandler())
-app.use(Raven.errorHandler())
-app.use(function onError (err, req, res, next) {
-  console.error(err.message)
-  res.status(500).end(`${res.sentry} ${err.message}` + '\n')
-})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/joke', authorize, status)
+app.get('/', authorize, status)
 
-app.post('/joke', authorize, joker)
+// app.post('/tone', authorize, joker)
+
+app.post('/tone', authorize, tonyTheTiger)
 
 function authorize ({ body }, res, next) {
   if (DRIFT_VERIFICATION_TOKEN !== body.token) {
     return res.status(401).send('Not authorized')
   }
   return next()
+}
+
+function tonyTheTiger (req, res, next) {
+  toneAnalyzer.tone(req.body, function (err, data) {
+    if (err) {
+      return next(err)
+    }
+    return res.json(data)
+  })
 }
 
 function status (_, response) {
@@ -38,18 +37,12 @@ function status (_, response) {
 
 function joker ({ body }, response) {
   const { type, orgId, data } = body
-  debug('body %O', body)
 
   if (type === 'new_message') {
     const kickoff = '<p>Okay, getting a joke asap!</p>'
     if (data.body === kickoff) {
       handleMessage(data, orgId)
     }
-    // handle search text
-    // if (data.body.includes('/joke ')) {
-    //   debug('data.body', data.body)
-    //   // handleMessage(data, orgId)
-    // }
   }
   return response.send('ok')
 }
